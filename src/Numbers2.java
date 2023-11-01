5d3d865bc1c8af395b46d7178b7bbc5dd8e957e6
Norayr <asoyannorayr99@gmail.com>
2023-11-01 19:55:47 +0300

    add

--- src/Numbers2.java
+++ src/Numbers2.java
@@ -1,15 +1,26 @@
+import java.util.Arrays;
+
 public class Numbers2 {
     public static void main(String[] args) {
-        Numbers2 result = getSubArrayBetween(0,1,2);
-        System.out.println(result);
+        int[] rename = {4, 8, 9, 30, 50};
+        int start = 1;
+        int end = 40;
 
+        System.out.println(Arrays.toString(getSubArrayBetween(rename, start, end)));
     }
     public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
         int a = 0;
-        int[] result = new int[a];
         for (int b = 0; b <= numbers.length; b++) {
             if (numbers[b] >= start && numbers[b] <= end) {
-                result[a] = numbers[b];
+                a++;
+            }
+        }
+        int[] result =new int[a];
+        int index = 0;
+        for (int b = 0; b <= numbers.length; b++) {
+            if (numbers[b] >= start && numbers[b] <= end) {
+                result[index] = numbers[b];
+                index++;
             }
         }
         return result;
