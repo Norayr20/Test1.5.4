e9a8eb2a99db3ce6b052d1b16dfe5cbf5696f2c4
Norayr <asoyannorayr99@gmail.com>
2023-11-11 14:49:54 +0300

    add

--- src/Numbers2.java
+++ src/Numbers2.java
@@ -2,27 +2,35 @@ import java.util.Arrays;
 
 public class Numbers2 {
     public static void main(String[] args) {
-        int[] rename = {4, 8, 9, 30, 50};
-        int start = 1;
-        int end = 40;
+        int[] rename = {-1,0,1};
+        int start = 0;
+        int end = 0;
 
         System.out.println(Arrays.toString(getSubArrayBetween(rename, start, end)));
     }
+
     public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
-        int a = 0;
-        for (int b = 0; b <= numbers.length; b++) {
-            if (numbers[b] >= start && numbers[b] <= end) {
-                a++;
+        int startIndex = -1;
+        int endIndex = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == start) {
+                startIndex = i;
+            }
+            if (numbers[i] == end) {
+                endIndex = i;
             }
         }
-        int[] result =new int[a];
+
+        if (startIndex == -1 || endIndex == -1) {
+            return new int[0];
+        }
+        int[] subArray = new int[endIndex - startIndex + 1];
         int index = 0;
-        for (int b = 0; b <= numbers.length; b++) {
-            if (numbers[b] >= start && numbers[b] <= end) {
-                result[index] = numbers[b];
-                index++;
-            }
+        for (int i = startIndex; i <= endIndex; i++) {
+            subArray[index] = numbers[i];
+            index++;
         }
-        return result;
+        return subArray;
     }
 }
